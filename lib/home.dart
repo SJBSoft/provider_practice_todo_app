887f525044e9e85e739802499938163edf067477
Sajeeb Shahriar <sajeeb3g@gmail.com>
2023-05-26 07:59:36 +0600

    Convert to stateless

--- lib/home.dart
+++ lib/home.dart
@@ -4,17 +4,12 @@ import 'package:provider/provider.dart';
 import 'package:provider_practice/model/todo_model.dart';
 import 'package:provider_practice/provider/todo_provider.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   final _textController = TextEditingController();
 
-  Future<void> _showDialogs() async {
+  Future<void> _showDialogs(BuildContext context) async {
     return showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -23,17 +18,22 @@ class _HomePageState extends State<HomePage> {
             content: TextField(
               controller: _textController,
               decoration: const InputDecoration(hintText: "Write todo Item"),
-            ), actions: [
-            TextButton(onPressed: () {
-              Navigator.pop(context);
-            }, child: const Text('Cancel')),
-            TextButton(onPressed: () {
-              context.read<TodoProvider>().addTodoList(
-                  TodoModel(title: _textController.text, isCompleted: false));
-              _textController.clear();
-              Navigator.pop(context);
-            }, child: const Text('Submit')),
-          ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    context.read<TodoProvider>().addTodoList(TodoModel(
+                        title: _textController.text, isCompleted: false));
+                    _textController.clear();
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Submit')),
+            ],
           );
         });
   }
@@ -47,21 +47,21 @@ class _HomePageState extends State<HomePage> {
           children: [
             Expanded(
                 child: Container(
-                  decoration: const BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(40),
-                          bottomLeft: Radius.circular(40))),
-                  child: const Center(
-                    child: Text(
-                      'TO DO List',
-                      style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ),
-                )),
+              decoration: const BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(40),
+                      bottomLeft: Radius.circular(40))),
+              child: const Center(
+                child: Text(
+                  'TO DO List',
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            )),
             Expanded(
                 flex: 3,
                 child: ListView.builder(
@@ -69,37 +69,43 @@ class _HomePageState extends State<HomePage> {
                     itemBuilder: (context, index) {
                       return ListTile(
                         onTap: () {
-                          provider.todoStatusChange(
-                              provider.allTodoList[index]);
+                          provider
+                              .todoStatusChange(provider.allTodoList[index]);
                         },
                         leading: MSHCheckbox(
                           size: 30,
                           value: provider.allTodoList[index].isCompleted,
-                          colorConfig: MSHColorConfig
-                              .fromCheckedUncheckedDisabled(
-                              checkedColor: Colors.blue
-                          ),
+                          colorConfig:
+                              MSHColorConfig.fromCheckedUncheckedDisabled(
+                                  checkedColor: Colors.blue),
                           style: MSHCheckboxStyle.stroke,
                           onChanged: (bool selected) {
-                            provider.todoStatusChange(
-                                provider.allTodoList[index]);
+                            provider
+                                .todoStatusChange(provider.allTodoList[index]);
                           },
                         ),
                         title: Text(
-                          provider.allTodoList[index].title, style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          decoration: provider.allTodoList[index].isCompleted
-                              ? TextDecoration.combine(
-                              [
-                                TextDecoration.underline,
-                                TextDecoration.lineThrough
-                              ]) : null
+                          provider.allTodoList[index].title,
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              decoration:
+                                  provider.allTodoList[index].isCompleted
+                                      ? TextDecoration.combine([
+                                          TextDecoration.underline,
+                                          TextDecoration.lineThrough
+                                        ])
+                                      : null),
                         ),
-                        ),
-                        trailing: IconButton(onPressed: (){
-                          provider.removeTodoList(provider.allTodoList[index]);
-                        }, icon: const Icon(Icons.delete_forever, color: Colors.red,)),
+                        trailing: IconButton(
+                            onPressed: () {
+                              provider
+                                  .removeTodoList(provider.allTodoList[index]);
+                            },
+                            icon: const Icon(
+                              Icons.delete_forever,
+                              color: Colors.red,
+                            )),
                       );
                     }))
           ],
@@ -108,7 +114,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple,
         onPressed: () {
-          _showDialogs();
+          _showDialogs(context);
         },
         child: const Icon(Icons.add, color: Colors.white),
       ),
